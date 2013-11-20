773b0740e6fb73198c3097b8d86e49cfb237c45f
saasbook <saasbook@saasbook.(none)>
2013-11-20 09:52:26 -0800

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Facebook::Application.config.secret_token = 'd2d1c2aa51d4a1a3e02e5bf896c4533d79cfff259bab00f3e5a412b47afd9ddb8fd5a254f3d6dc72edf0207fc32f6197e1dd30a6b6893e7fd577a04e9f13cd27'
